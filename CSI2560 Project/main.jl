97193740688e55170ac66a91cb332d599414bb71
AnthonyZolman <161530910+AnthonyZolman@users.noreply.github.com>
2026-03-16 17:03:04 -0400

    File is now started with module imports and one defined function

---
+++ CSI2560 Project/main.jl
@@ -0,0 +1,8 @@
+#Assists in building sparse arrays without saving 
+#lots of zeros in memory - easier calculations
+using SparseArrays
+#Reads .data files as 'delimited' text file
+using DelimitedFiles
+
+#Finds cosine product between two vectors
+function cosine() end
